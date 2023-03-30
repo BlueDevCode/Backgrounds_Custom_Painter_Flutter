0bdb8fa1de1fcac5f1253ebdc55ec442f70bc3b0
Claudia Cadec <claudiacadec@outlook.com>
2023-03-30 21:36:18 +0200

    commit

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -59,7 +59,7 @@ class _HeaderDiagonalPainter extends CustomPainter{
     lapiz.style = PaintingStyle.fill;
     lapiz.strokeWidth = 2;
     
-    final path = new Path();
+    final path = Path();
     path.moveTo( 0, size.height * 0.35 );
     path.lineTo(size.width, size.height * 0.30);
     path.lineTo(size.width, 0);
@@ -217,6 +217,61 @@ class _HeaderWavePainter extends CustomPainter{
 
     final lapiz = Paint();
     lapiz.color = const Color.fromARGB(255, 32, 12, 67);
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 20;
+    
+    final path = Path();
+    
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.30, size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.20, size.width, size.height * 0.25);
+    path.lineTo(size.width , 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+  @override  
+  bool shouldRepaint(CustomPainter oldDelegate){
+    return true;
+  }
+
+
+
+}
+class HeaderWaveGradient extends StatelessWidget {
+  const HeaderWaveGradient
+({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+    
+      child: CustomPaint(
+        painter:_HeaderWavePainter() ,
+        ),
+    );
+  }
+}
+class _HeaderWaveGradientPainter extends CustomPainter{
+  @override
+  void paint (Canvas canvas, Size size){
+
+    final Rect rect = Rect.fromCircle(
+      center: const Offset(150.0 , 55.0),
+      radius: 180
+    );
+
+    const Gradient gradiente = LinearGradient(
+      colors: <Color>[
+       Color.fromARGB(255, 105, 18, 211),
+       Color.fromARGB(255, 163, 28, 212),
+       Color.fromARGB(255, 82, 34, 147),
+         ]
+      );
+
+    final lapiz = Paint() ..shader = gradiente.createShader(rect);
+    //lapiz.color = Colors.red;
     lapiz.style = PaintingStyle.fill;
     lapiz.strokeWidth = 20;
     
